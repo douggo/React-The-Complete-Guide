5f7bf3718e73ed880b383a63689ae8b061a0a145
Douglas Felipe da Silva <douglas.silva@unidavi.edu.br>
2022-04-22 23:01:16 -0300

    learning about props object

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem date='19/03/2022' description='Car Insurance' value='294,67' />
+      <ExpenseItem date='25/03/2022' description='Guitar' value='100' />
+      <ExpenseItem date='30/03/2022' description='Pizza' value='5,55' />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,13 @@
 import React from "react";
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
   return (
     <div className='expense-item'>
-      <div>March 28 2022</div>
+      <div>{props.date}</div>
       <div className='expense-item__description'>
-        <h2>Car Insurance</h2>
-        <div className='expense-item__price'>$294.67</div>
+        <h2>{props.description}</h2>
+        <div className='expense-item__price'>${props.value}</div>
       </div>
     </div>
   );
