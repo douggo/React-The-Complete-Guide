3d87ac668a6e850f3d6924a636757ba57d5d03c2
Douglas Felipe da Silva <douglas.silva@unidavi.edu.br>
2022-04-28 10:26:16 -0300

    learning to add new expenses to the expense list

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
+import { useState } from "react";
 import ExpenseList from "./components/Expenses/ExpenseList";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {
+    key         : 1,
+    description : 'Car Insurance',
+    date        : new Date(2020, 2, 19),
+    value       : 264.97
+  },
+  {
+    key         : 2,
+    description : 'Guitar Reformation',
+    date        : new Date(2020, 2, 20),
+    value       : 165.99
+  },
+  {
+    key         : 3,
+    description : 'Supermarket',
+    date        : new Date(2020, 2, 21),
+    value       : 300.00
+  }
+];
+
 function App() {
-  const expenses = [
-    {
-      key         : 1,
-      description : 'Car Insurance',
-      date        : new Date(2020, 2, 19),
-      value       : 264.97
-    },
-    {
-      key         : 2,
-      description : 'Guitar Reformation',
-      date        : new Date(2020, 2, 20),
-      value       : 165.99
-    },
-    {
-      key         : 3,
-      description : 'Supermarket',
-      date        : new Date(2020, 2, 21),
-      value       : 300.00
-    }
-  ];
+  
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = expense => {
-    console.log('In App.js:');
-    console.log(expense);
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses]
+    });
   }
 
   return (
